kolab-groupware/baloo

Akonadi-Indexer: Collection indexing
Index that allows to search for collections.

src/pim/agent/agent.cpp
@@ -28,15 +28,13 @@
 #include "akonotesindexer.h"
 #include "calendarindexer.h"
 #include "balooindexeradaptor.h"
+#include "collectionupdatejob.h"
 
 #include "src/file/priority.h"
 
-#include <Akonadi/ItemFetchJob>
-#include <Akonadi/ItemFetchScope>
 #include <Akonadi/ChangeRecorder>
-#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/ItemFetchScope>
 #include <Akonadi/CollectionFetchScope>
-#include <Akonadi/CollectionStatistics>
 #include <Akonadi/AgentManager>
 #include <Akonadi/ServerManager>
 
@@ -86,7 +84,9 @@ BalooIndexingAgent::BalooIndexingAgent(const QString& id)
     changeRecorder()->itemFetchScope().setAncestorRetrieval(Akonadi::ItemFetchScope::Parent);
     changeRecorder()->itemFetchScope().setFetchRemoteIdentification(false);
     changeRecorder()->itemFetchScope().setFetchModificationTime(false);
+    changeRecorder()->collectionFetchScope().setAncestorRetrieval(Akonadi::CollectionFetchScope::All);
     changeRecorder()->setChangeRecordingEnabled(false);
+    changeRecorder()->fetchCollection(true);
 
     new BalooIndexerAdaptor(this);
 
@@ -105,6 +105,15 @@ BalooIndexingAgent::BalooIndexingAgent(const QString& id)
 
 BalooIndexingAgent::~BalooIndexingAgent()
 {
+}
+
+void BalooIndexingAgent::reindexAll()
+{
+    kDebug() << "Reindexing everything";
+    m_scheduler.abort();
+    m_index.removeDatabase();
+    m_index.createIndexers();
+    QTimer::singleShot(0, &m_scheduler, SLOT(scheduleCompleteSync()));
 }
 
 void BalooIndexingAgent::reindexCollection(const qlonglong id)
@@ -156,18 +165,38 @@ void BalooIndexingAgent::itemsRemoved(const Akonadi::Item::List& items)
     m_index.scheduleCommit();
 }
 
-void BalooIndexingAgent::collectionRemoved(const Akonadi::Collection& collection)
-{
-    m_index.remove(collection);
-    m_index.scheduleCommit();
-}
-
 void BalooIndexingAgent::itemsMoved(const Akonadi::Item::List& items,
                                     const Akonadi::Collection& sourceCollection,
                                     const Akonadi::Collection& destinationCollection)
 {
     m_index.move(items, sourceCollection, destinationCollection);
     m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionAdded(const Akonadi::Collection& collection, const Akonadi::Collection& parent)
+{
+    m_index.index(collection);
+    m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionChanged(const Akonadi::Collection& collection)
+{
+    CollectionUpdateJob *job = new CollectionUpdateJob(m_index, collection, this);
+    job->start();
+}
+
+void BalooIndexingAgent::collectionRemoved(const Akonadi::Collection& collection)
+{
+    m_index.remove(collection);
+    m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionMoved(const Akonadi::Collection &collection, const Akonadi::Collection &collectionSource,
+                                     const Akonadi::Collection &collectionDestination)
+{
+    m_index.remove(collection);
+    CollectionUpdateJob *job = new CollectionUpdateJob(m_index, collection, this);
+    job->start();
 }
 
 void BalooIndexingAgent::cleanup()

src/pim/agent/index.cpp
@@ -32,34 +32,9 @@
 #include <xapian/query.h>
 #include <xapian/enquire.h>
 
-
-namespace {
-    QString dbPath(const QString& dbName) {
-        QString basePath = QLatin1String("baloo");
-        if (Akonadi::ServerManager::hasInstanceIdentifier()) {
-            basePath = QString::fromLatin1("baloo/instances/%1").arg(Akonadi::ServerManager::instanceIdentifier());
-        }
-        return KGlobal::dirs()->localxdgdatadir() + QString::fromLatin1("%1/%2/").arg(basePath, dbName);
-    }
-    QString emailIndexingPath() {
-        return dbPath(QLatin1String("email"));
-    }
-    QString contactIndexingPath() {
-        return dbPath(QLatin1String("contacts"));
-    }
-    QString emailContactsIndexingPath() {
-        return dbPath(QLatin1String("emailContacts"));
-    }
-    QString akonotesIndexingPath() {
-        return dbPath(QLatin1String("notes"));
-    }
-    QString calendarIndexingPath() {
-        return dbPath(QLatin1String("calendars"));
-    }
-}
-
 Index::Index(QObject* parent)
-: QObject(parent)
+: QObject(parent),
+  m_collectionIndexer(0)
 {
     m_commitTimer.setInterval(1000);
     m_commitTimer.setSingleShot(true);
@@ -69,6 +44,8 @@ Index::Index(QObject* parent)
 
 Index::~Index()
 {
+    delete m_collectionIndexer;
+    m_collectionIndexer = 0;
     qDeleteAll(m_indexer.values().toSet());
     m_indexer.clear();
 }
@@ -91,12 +68,18 @@ static void removeDir(const QString& dirName)
 
 void Index::removeDatabase()
 {
+    delete m_collectionIndexer;
+    m_collectionIndexer = 0;
+    qDeleteAll(m_indexer.values().toSet());
+    m_indexer.clear();
+
     kDebug() << "Removing database";
     removeDir(emailIndexingPath());
     removeDir(contactIndexingPath());
     removeDir(emailContactsIndexingPath());
     removeDir(akonotesIndexingPath());
     removeDir(calendarIndexingPath());
+    removeDir(collectionIndexingPath());
 }
 
 AbstractIndexer* Index::indexerForItem(const Akonadi::Item& item) const
@@ -196,14 +179,47 @@ void Index::remove(const Akonadi::Item::List& items)
     }
 }
 
+void Index::index(const Akonadi::Collection& collection)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->index(collection);
+        m_collectionIndexer->commit();
+    }
+    kDebug() << "indexed " << collection.id();
+}
+
+void Index::change(const Akonadi::Collection& col)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->change(col);
+        m_collectionIndexer->commit();
+    }
+}
+
 void Index::remove(const Akonadi::Collection& col)
 {
+    //Remove items
     Q_FOREACH (AbstractIndexer *indexer, indexersForMimetypes(col.contentMimeTypes())) {
         try {
             indexer->remove(col);
         } catch (const Xapian::Error &e) {
             kWarning() << "Xapian error in indexer" << indexer << ":" << e.get_msg().c_str();
         }
+    }
+
+    if (m_collectionIndexer) {
+        m_collectionIndexer->remove(col);
+        m_collectionIndexer->commit();
+    }
+}
+
+void Index::move(const Akonadi::Collection& collection,
+            const Akonadi::Collection& from,
+            const Akonadi::Collection& to)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->move(collection, from, to);
+        m_collectionIndexer->commit();
     }
 }
 
@@ -267,10 +283,25 @@ bool Index::createIndexers()
     }
     catch (const Xapian::DatabaseError &e) {
         delete indexer;
-        kError() << "Failed to create akonotes indexer:" << QString::fromStdString(e.get_msg());
+        kError() << "Failed to create collection indexer:" << QString::fromStdString(e.get_msg());
     }
     catch (...) {
         delete indexer;
+        kError() << "Random exception, but we do not want to crash";
+    }
+
+    try {
+        QDir().mkpath(collectionIndexingPath());
+        m_collectionIndexer = new CollectionIndexer(collectionIndexingPath());
+    }
+    catch (const Xapian::DatabaseError &e) {
+        delete m_collectionIndexer;
+        m_collectionIndexer = 0;
+        kError() << "Failed to create akonotes indexer:" << QString::fromStdString(e.get_msg());
+    }
+    catch (...) {
+        delete m_collectionIndexer;
+        m_collectionIndexer = 0;
         kError() << "Random exception, but we do not want to crash";
     }
 
@@ -345,3 +376,51 @@ qlonglong Index::indexedItemsInDatabase(const std::string& term, const QString& 
     }
     return db.get_termfreq(term);
 }
+
+void Index::setOverrideDbPrefixPath(const QString& path)
+{
+    m_overridePrefixPath = path;
+}
+
+QString Index::dbPath(const QString& dbName) const
+{
+    if (!m_overridePrefixPath.isEmpty()) {
+        return QString::fromLatin1("%1/%2/").arg(m_overridePrefixPath, dbName);
+    }
+    QString basePath = QLatin1String("baloo");
+    if (Akonadi::ServerManager::hasInstanceIdentifier()) {
+        basePath = QString::fromLatin1("baloo/instances/%1").arg(Akonadi::ServerManager::instanceIdentifier());
+    }
+    return KGlobal::dirs()->localxdgdatadir() + QString::fromLatin1("%1/%2/").arg(basePath, dbName);
+}
+
+QString Index::emailIndexingPath() const
+{
+    return dbPath(QLatin1String("email"));
+}
+
+QString Index::contactIndexingPath() const
+{
+    return dbPath(QLatin1String("contacts"));
+}
+
+QString Index::emailContactsIndexingPath() const
+{
+    return dbPath(QLatin1String("emailContacts"));
+}
+
+QString Index::akonotesIndexingPath() const
+{
+    return dbPath(QLatin1String("notes"));
+}
+
+QString Index::calendarIndexingPath() const
+{
+    return dbPath(QLatin1String("calendars"));
+}
+
+QString Index::collectionIndexingPath() const
+{
+    return dbPath(QLatin1String("collections"));
+}
+

src/pim/lib/collectionquery.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the KDE Baloo Project
- * Copyright (C) 2013  Vishesh Handa <me@vhanda.in>
+ * Copyright (C) 2014  Christian Mollekopf <mollekopf@kolabsys.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,40 +20,57 @@
  *
  */
 
-#ifndef RESULT_ITERATOR_H
-#define RESULT_ITERATOR_H
+#ifndef _COLLECTION_QUERY_H
+#define _COLLECTION_QUERY_H
 
 #include "pim_export.h"
+#include "query.h"
+#include "resultiterator.h"
 
-#include <Akonadi/Item>
+#include <QStringList>
+#include <Akonadi/Collection>
 
 namespace Baloo {
 namespace PIM {
 
-class ContactQuery;
-class EmailQuery;
-class NoteQuery;
-
-class BALOO_PIM_EXPORT ResultIterator
+class BALOO_PIM_EXPORT CollectionQuery : public Query
 {
 public:
-    ResultIterator();
-    ResultIterator(const ResultIterator& ri);
-    ~ResultIterator();
+    CollectionQuery();
+    virtual ~CollectionQuery();
 
-    Akonadi::Item::Id id();
-    bool next();
+    void setNamespace(const QStringList &ns);
+    void setMimetype(const QStringList &mt);
+
+    /**
+     * Matches the string \p match in the name.
+     */
+    void nameMatches(const QString& match);
+    void identifierMatches(const QString& match);
+    void pathMatches(const QString& match);
+
+    void setLimit(int limit);
+    int limit() const;
+
+    /**
+     * Execute the query and return an iterator to fetch
+     * the results
+     */
+    ResultIterator exec();
+
+    /**
+     * For testing
+     */
+    void setDatabaseDir(const QString &dir);
 
 private:
-    friend class ContactQuery;
-    friend class EmailQuery;
-    friend class NoteQuery;
-    friend class CollectionQuery;
-
+    //@cond PRIVATE
     class Private;
-    Private* d;
+    Private * const d;
+    //@endcond
 };
 
 }
 }
-#endif // RESULT_ITERATOR_H
+
+#endif // _COLLECTION_QUERY_H

KDE/akonadi-search

agent/agent.cpp
@@ -28,15 +28,13 @@
 #include "akonotesindexer.h"
 #include "calendarindexer.h"
 #include "balooindexeradaptor.h"
+#include "collectionupdatejob.h"
 
 #include "priority.h"
 
-#include <ItemFetchJob>
-#include <ItemFetchScope>
 #include <ChangeRecorder>
-#include <CollectionFetchJob>
 #include <AkonadiCore/CollectionFetchScope>
-#include <AkonadiCore/CollectionStatistics>
+#include <AkonadiCore/ItemFetchScope>
 
 #include <AgentManager>
 #include <ServerManager>
@@ -89,7 +87,9 @@ BalooIndexingAgent::BalooIndexingAgent(const QString& id)
     changeRecorder()->itemFetchScope().setAncestorRetrieval(Akonadi::ItemFetchScope::Parent);
     changeRecorder()->itemFetchScope().setFetchRemoteIdentification(false);
     changeRecorder()->itemFetchScope().setFetchModificationTime(false);
+    changeRecorder()->collectionFetchScope().setAncestorRetrieval(Akonadi::CollectionFetchScope::All);
     changeRecorder()->setChangeRecordingEnabled(false);
+    changeRecorder()->fetchCollection(true);
 
     new BalooIndexerAdaptor(this);
 
@@ -108,6 +108,15 @@ BalooIndexingAgent::BalooIndexingAgent(const QString& id)
 
 BalooIndexingAgent::~BalooIndexingAgent()
 {
+}
+
+void BalooIndexingAgent::reindexAll()
+{
+    kDebug() << "Reindexing everything";
+    m_scheduler.abort();
+    m_index.removeDatabase();
+    m_index.createIndexers();
+    QTimer::singleShot(0, &m_scheduler, SLOT(scheduleCompleteSync()));
 }
 
 void BalooIndexingAgent::reindexCollection(const qlonglong id)
@@ -160,18 +169,38 @@ void BalooIndexingAgent::itemsRemoved(const Akonadi::Item::List& items)
     m_index.scheduleCommit();
 }
 
-void BalooIndexingAgent::collectionRemoved(const Akonadi::Collection& collection)
-{
-    m_index.remove(collection);
-    m_index.scheduleCommit();
-}
-
 void BalooIndexingAgent::itemsMoved(const Akonadi::Item::List& items,
                                     const Akonadi::Collection& sourceCollection,
                                     const Akonadi::Collection& destinationCollection)
 {
     m_index.move(items, sourceCollection, destinationCollection);
     m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionAdded(const Akonadi::Collection& collection, const Akonadi::Collection& parent)
+{
+    m_index.index(collection);
+    m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionChanged(const Akonadi::Collection& collection)
+{
+    CollectionUpdateJob *job = new CollectionUpdateJob(m_index, collection, this);
+    job->start();
+}
+
+void BalooIndexingAgent::collectionRemoved(const Akonadi::Collection& collection)
+{
+    m_index.remove(collection);
+    m_index.scheduleCommit();
+}
+
+void BalooIndexingAgent::collectionMoved(const Akonadi::Collection &collection, const Akonadi::Collection &collectionSource,
+                                     const Akonadi::Collection &collectionDestination)
+{
+    m_index.remove(collection);
+    CollectionUpdateJob *job = new CollectionUpdateJob(m_index, collection, this);
+    job->start();
 }
 
 void BalooIndexingAgent::cleanup()

agent/index.cpp
@@ -32,34 +32,9 @@
 #include <xapian/query.h>
 #include <xapian/enquire.h>
 
-
-namespace {
-    QString dbPath(const QString& dbName) {
-        QString basePath = QLatin1String("baloo");
-        if (Akonadi::ServerManager::hasInstanceIdentifier()) {
-            basePath = QString::fromLatin1("baloo/instances/%1").arg(Akonadi::ServerManager::instanceIdentifier());
-        }
-        return KGlobal::dirs()->localxdgdatadir() + QString::fromLatin1("%1/%2/").arg(basePath, dbName);
-    }
-    QString emailIndexingPath() {
-        return dbPath(QLatin1String("email"));
-    }
-    QString contactIndexingPath() {
-        return dbPath(QLatin1String("contacts"));
-    }
-    QString emailContactsIndexingPath() {
-        return dbPath(QLatin1String("emailContacts"));
-    }
-    QString akonotesIndexingPath() {
-        return dbPath(QLatin1String("notes"));
-    }
-    QString calendarIndexingPath() {
-        return dbPath(QLatin1String("calendars"));
-    }
-}
-
 Index::Index(QObject* parent)
-: QObject(parent)
+: QObject(parent),
+  m_collectionIndexer(Q_NULLPTR)
 {
     m_commitTimer.setInterval(1000);
     m_commitTimer.setSingleShot(true);
@@ -69,6 +44,8 @@ Index::Index(QObject* parent)
 
 Index::~Index()
 {
+    delete m_collectionIndexer;
+    m_collectionIndexer = Q_NULLPTR;
     qDeleteAll(m_indexer.values().toSet());
     m_indexer.clear();
 }
@@ -91,12 +68,18 @@ static void removeDir(const QString& dirName)
 
 void Index::removeDatabase()
 {
+    delete m_collectionIndexer;
+    m_collectionIndexer = Q_NULLPTR;
+    qDeleteAll(m_indexer.values().toSet());
+    m_indexer.clear();
+
     qDebug() << "Removing database";
     removeDir(emailIndexingPath());
     removeDir(contactIndexingPath());
     removeDir(emailContactsIndexingPath());
     removeDir(akonotesIndexingPath());
     removeDir(calendarIndexingPath());
+    removeDir(collectionIndexingPath());
 }
 
 AbstractIndexer* Index::indexerForItem(const Akonadi::Item& item) const
@@ -196,14 +179,47 @@ void Index::remove(const Akonadi::Item::List& items)
     }
 }
 
+void Index::index(const Akonadi::Collection& collection)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->index(collection);
+        m_collectionIndexer->commit();
+    }
+    qDebug() << "indexed " << collection.id();
+}
+
+void Index::change(const Akonadi::Collection& col)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->change(col);
+        m_collectionIndexer->commit();
+    }
+}
+
 void Index::remove(const Akonadi::Collection& col)
 {
+    //Remove items
     Q_FOREACH (AbstractIndexer *indexer, indexersForMimetypes(col.contentMimeTypes())) {
         try {
             indexer->remove(col);
         } catch (const Xapian::Error &e) {
             qWarning() << "Xapian error in indexer" << indexer << ":" << e.get_msg().c_str();
         }
+    }
+
+    if (m_collectionIndexer) {
+        m_collectionIndexer->remove(col);
+        m_collectionIndexer->commit();
+    }
+}
+
+void Index::move(const Akonadi::Collection& collection,
+            const Akonadi::Collection& from,
+            const Akonadi::Collection& to)
+{
+    if (m_collectionIndexer) {
+        m_collectionIndexer->move(collection, from, to);
+        m_collectionIndexer->commit();
     }
 }
 
@@ -217,7 +233,7 @@ void Index::addIndexer(AbstractIndexer* indexer)
 
 bool Index::createIndexers()
 {
-    AbstractIndexer *indexer = 0;
+    AbstractIndexer *indexer = Q_NULLPTR;
     try {
         QDir().mkpath(emailIndexingPath());
         QDir().mkpath(emailContactsIndexingPath());
@@ -272,6 +288,21 @@ bool Index::createIndexers()
     }
     catch (...) {
         delete indexer;
+        qCritical() << "Random exception, but we do not want to crash";
+    }
+
+    try {
+        QDir().mkpath(collectionIndexingPath());
+        m_collectionIndexer = new CollectionIndexer(collectionIndexingPath());
+    }
+    catch (const Xapian::DatabaseError &e) {
+        delete m_collectionIndexer;
+        m_collectionIndexer = Q_NULLPTR;
+        qCritical() << "Failed to create akonotes indexer:" << QString::fromStdString(e.get_msg());
+    }
+    catch (...) {
+        delete m_collectionIndexer;
+        m_collectionIndexer = Q_NULLPTR;
         qCritical() << "Random exception, but we do not want to crash";
     }
 
@@ -346,3 +377,51 @@ qlonglong Index::indexedItemsInDatabase(const std::string& term, const QString& 
     }
     return db.get_termfreq(term);
 }
+
+void Index::setOverrideDbPrefixPath(const QString& path)
+{
+    m_overridePrefixPath = path;
+}
+
+QString Index::dbPath(const QString& dbName) const
+{
+    if (!m_overridePrefixPath.isEmpty()) {
+        return QString::fromLatin1("%1/%2/").arg(m_overridePrefixPath, dbName);
+    }
+    QString basePath = QLatin1String("baloo");
+    if (Akonadi::ServerManager::hasInstanceIdentifier()) {
+        basePath = QString::fromLatin1("baloo/instances/%1").arg(Akonadi::ServerManager::instanceIdentifier());
+    }
+    return KGlobal::dirs()->localxdgdatadir() + QString::fromLatin1("%1/%2/").arg(basePath, dbName);
+}
+
+QString Index::emailIndexingPath() const
+{
+    return dbPath(QLatin1String("email"));
+}
+
+QString Index::contactIndexingPath() const
+{
+    return dbPath(QLatin1String("contacts"));
+}
+
+QString Index::emailContactsIndexingPath() const
+{
+    return dbPath(QLatin1String("emailContacts"));
+}
+
+QString Index::akonotesIndexingPath() const
+{
+    return dbPath(QLatin1String("notes"));
+}
+
+QString Index::calendarIndexingPath() const
+{
+    return dbPath(QLatin1String("calendars"));
+}
+
+QString Index::collectionIndexingPath() const
+{
+    return dbPath(QLatin1String("collections"));
+}
+

lib/collectionquery.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the KDE Baloo Project
- * Copyright (C) 2013  Vishesh Handa <me@vhanda.in>
+ * Copyright (C) 2014  Christian Mollekopf <mollekopf@kolabsys.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,40 +20,57 @@
  *
  */
 
-#ifndef RESULT_ITERATOR_H
-#define RESULT_ITERATOR_H
+#ifndef _COLLECTION_QUERY_H
+#define _COLLECTION_QUERY_H
 
 #include "pim_export.h"
+#include "query.h"
+#include "resultiterator.h"
 
-#include <Item>
+#include <QStringList>
+#include <AkonadiCore/Collection>
 
 namespace Baloo {
 namespace PIM {
 
-class ContactQuery;
-class EmailQuery;
-class NoteQuery;
-
-class BALOO_PIM_EXPORT ResultIterator
+class BALOO_PIM_EXPORT CollectionQuery : public Query
 {
 public:
-    ResultIterator();
-    ResultIterator(const ResultIterator& ri);
-    ~ResultIterator();
+    CollectionQuery();
+    virtual ~CollectionQuery();
 
-    Akonadi::Item::Id id();
-    bool next();
+    void setNamespace(const QStringList &ns);
+    void setMimetype(const QStringList &mt);
+
+    /**
+     * Matches the string \p match in the name.
+     */
+    void nameMatches(const QString& match);
+    void identifierMatches(const QString& match);
+    void pathMatches(const QString& match);
+
+    void setLimit(int limit);
+    int limit() const;
+
+    /**
+     * Execute the query and return an iterator to fetch
+     * the results
+     */
+    ResultIterator exec();
+
+    /**
+     * For testing
+     */
+    void setDatabaseDir(const QString &dir);
 
 private:
-    friend class ContactQuery;
-    friend class EmailQuery;
-    friend class NoteQuery;
-    friend class CollectionQuery;
-
+    //@cond PRIVATE
     class Private;
-    Private* d;
+    Private * const d;
+    //@endcond
 };
 
 }
 }
-#endif // RESULT_ITERATOR_H
+
+#endif // _COLLECTION_QUERY_H