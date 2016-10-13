KDE/akonadi-search

commit 1e70d63a9439f48b5f1a70accac531a10f4e4239

Create AgePostingSource on heap
There was an undocumented behaviour change in Xapian 1.4 where
Xapian::Query() no longer internally creates a clone of the
PostingResource that we pass to it and instead takes a (shared)
ownership of the pointer that is then re-used later while
the actual query is being executed, which means that the
PostingResource must live at least until the query execution
is finished.

We were creating the AgePostingSource on stack, which lead to
use-after-free in Xapian 1.4.

BUG: 363741
FIXED-IN: 5.3.2

search/email/emailsearchstore.cpp
@@ -117,7 +117,6 @@ QString EmailSearchStore::text(int queryId)
 
 Xapian::Query EmailSearchStore::finalizeQuery(const Xapian::Query &query)
 {
-    AgePostingSource ps(0);
-    return Xapian::Query(Xapian::Query::OP_AND_MAYBE, query, Xapian::Query(&ps));
+    return Xapian::Query(Xapian::Query::OP_AND_MAYBE, query, Xapian::Query(new AgePostingSource(0)));
 }
 

commit 5aefe02b83a6d73dbd048337d2ba54fee4bbd67c

Fix a memory leak introduced in 1e70d63a943
CCBUG: 363741

search/email/emailsearchstore.cpp
@@ -117,6 +117,6 @@ QString EmailSearchStore::text(int queryId)
 
 Xapian::Query EmailSearchStore::finalizeQuery(const Xapian::Query &query)
 {
-    return Xapian::Query(Xapian::Query::OP_AND_MAYBE, query, Xapian::Query(new AgePostingSource(0)));
+    return Xapian::Query(Xapian::Query::OP_AND_MAYBE, query, Xapian::Query((new AgePostingSource(0))->release()));
 }
 