79123aa553fae154b8bc4f50b31a0f8c12b49308
Daniel Vrátil <dvratil@kde.org>
2017-10-30 23:01:53 +0100

    Initial implementation of Indexing plugin

---
+++ akonadiplugin/indexer/indexingplugin.cpp
@@ -0,0 +1,93 @@
+/*
+ * This file is part of the KDE Akonadi Search Project
+ * Copyright (C) 2017  Daniel Vrátil <dvratil@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "indexingplugin.h"
+#include "akonadiplugin_indexing_debug.h"
+
+#include "store.h"
+
+using namespace Akonadi::Search;
+
+IndexingPlugin::IndexingPlugin()
+{
+}
+
+IndexingPlugin::~IndexingPlugin()
+{
+    for (auto it = mStoreCache.begin(), end = mStoreCache.end(); it != end; ++it) {
+        for (const auto store : qAsConst(*it)) {
+            store->commit();
+            delete store;
+        }
+    }
+}
+
+bool IndexingPlugin::doIndex(const QString &mimeType, const IndexingFunc &indexFunc)
+{
+    auto it = mStoreCache.find(mimeType);
+    if (it == mStoreCache.end()) {
+        const auto stores = Store::create(mimeType);
+        if (stores.isEmpty()) {
+            qCWarning(AKONADIPLUGIN_INDEXING_LOG) << "No Store for type" << mimeType;
+            return false;
+        }
+        it = mStoreCache.insert(mimeType, stores);
+        for (auto store : stores) {
+            store->setAutoCommit(100, 2000);
+        }
+    }
+
+    bool result = false;
+    for (const auto &store : qAsConst(*it)) {
+        result |= indexFunc(store);
+    }
+    return result;
+}
+
+bool IndexingPlugin::index(const QString &mimeType, qint64 id, const QByteArray& rawData)
+{
+    return doIndex(mimeType, [id, &rawData](Store *store) {
+        return store->index(id, rawData);
+    });
+}
+
+bool IndexingPlugin::moveItem(const QString &mimeType, qint64 id, qint64 sourceCollection,
+                              qint64 destinationCollection)
+{
+    return doIndex(mimeType, [id, sourceCollection, destinationCollection](Store *store) {
+        return store->move(id, sourceCollection, destinationCollection);
+    });
+}
+
+bool IndexingPlugin::removeItem(const QString &mimeType, qint64 id)
+{
+    return doIndex(mimeType, [id](Store *store) {
+        return store->removeItem(id);
+    });
+}
+
+bool IndexingPlugin::removeCollection(const QString &mimeType, qint64 id)
+{
+    return doIndex(mimeType, [id](Store *store) {
+        return store->removeCollection(id);
+    });
+}
---
+++ akonadiplugin/indexer/indexingplugin.h
@@ -0,0 +1,68 @@
+/*
+ * This file is part of the KDE Akonadi Search Project
+ * Copyright (C) 2017  Daniel Vrátil <dvratil@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef AKONADI_SEARCH_INDEXING_PLUGIN_H_
+#define AKONADI_SEARCH_INDEXING_PLUGIN_H_
+
+#include <QObject>
+#include <akonadi/abstractindexingplugin.h>
+
+#include <QHash>
+#include <QVector>
+
+#include <functional>
+
+class QTimer;
+
+namespace Akonadi {
+namespace Search {
+
+class Store;
+
+class IndexingPlugin : public QObject,
+                       public Akonadi::Server::AbstractIndexingPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(Akonadi::Server::AbstractIndexingPlugin)
+    Q_PLUGIN_METADATA(IID "org.kde.akonadi.IndexingPlugin" FILE "akonadi_indexing_plugin.json")
+
+public:
+    explicit IndexingPlugin();
+    ~IndexingPlugin() override;
+
+    bool index(const QString &mimeType, qint64 id, const QByteArray &rawData) override;
+    bool removeItem(const QString &mimeType, qint64 id) override;
+    bool removeCollection(const QString &mimeType, qint64 id) override;
+    bool moveItem(const QString &mimeType, qint64 id, qint64 sourceCollection,
+                  qint64 destinationCollection) override;
+
+private:
+    using IndexingFunc = std::function<bool(Store*)>;
+    bool doIndex(const QString &mimeType, const IndexingFunc &indexFunc);
+
+    QMultiHash<QString, QVector<Store*>> mStoreCache;
+};
+
+}
+}
+
+#endif
