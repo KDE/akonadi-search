28985a5c26f6c153e6dfc675de93733c3cc9f36a
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-18 08:26:05 +0200

    contacts runner: Use K_PLUGIN_CLASS_WITH_JSON directly
    Task: https://phabricator.kde.org/T14542

--- runner/pimcontactsrunner.cpp
+++ runner/pimcontactsrunner.cpp
@@ -225,6 +225,6 @@ void PIMContactsRunner::run(const Plasma::RunnerContext &context, const Plasma::
     }
 }
 
-K_EXPORT_PLASMA_RUNNER_WITH_JSON(PIMContactsRunner, "plasma-krunner-pimcontacts.json")
+K_PLUGIN_CLASS_WITH_JSON(PIMContactsRunner, "plasma-krunner-pimcontacts.json")
 
 #include "pimcontactsrunner.moc"
