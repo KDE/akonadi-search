0d90d4698ec2f48f458104aacfb32aa57848e8e2
Laurent Montel <montel@kde.org>
2022-01-01 16:09:31 +0100

    GIT_SILENT: happy new year

--- lib/indexeditems.h
+++ lib/indexeditems.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of the KDE Akonadi Search Project
- * SPDX-FileCopyrightText: 2016-2021 Laurent Montel <montel@kde.org>
+ * SPDX-FileCopyrightText: 2016-2022 Laurent Montel <montel@kde.org>
  *
  * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
  *
