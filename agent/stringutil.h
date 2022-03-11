437a98ed899cdc049e90b16cbdfb89ccf7cf717b
Laurent Montel <montel@kde.org>
2022-03-11 07:19:01 +0100

    Add normalize string

---
+++ agent/stringutil.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ *
+ */
+
+#pragma once
+#include <QString>
+
+namespace StringUtil
+{
+Q_REQUIRED_RESULT QString normalize(QStringView str);
+};
