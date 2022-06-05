4357c1d0ce5ef4daff496042c0dd9a015cd18404
l10n daemon script <scripty@kde.org>
2022-06-05 01:43:32 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- runner/plasma-krunner-pimcontacts.json.cmake
+++ runner/plasma-krunner-pimcontacts.json.cmake
@@ -3,15 +3,30 @@
         "Authors": [
             {
                 "Email": "dvratil@kde.org",
-                "Name": "Daniel Vrátil"
+                "Name": "Daniel Vrátil",
+                "Name[ca]": "Daniel Vrátil",
+                "Name[nl]": "Daniel Vrátil",
+                "Name[pt]": "Daniel Vrátil",
+                "Name[uk]": "Daniel Vrátil",
+                "Name[x-test]": "xxDaniel Vrátilxx"
             }
         ],
         "Description": "Runner which searches through your contacts",
+        "Description[ca]": "Executor que cerca a través dels vostres contactes",
+        "Description[nl]": "Programma dat uw contactpersonen doorzoekt",
+        "Description[pt]": "Um módulo de execução que pesquisa nos seus contactos",
+        "Description[uk]": "Засіб запуску для пошуку у записах ваших контактів",
+        "Description[x-test]": "xxRunner which searches through your contactsxx",
         "EnabledByDefault": true,
         "Icon": "view-pim-contacts",
         "Id": "PIM Contacts Search Runner",
         "License": "LGPL",
         "Name": "PIM Contacts Search",
+        "Name[ca]": "Cerca de contactes PIM",
+        "Name[nl]": "PIM in contactpersonen zoeken",
+        "Name[pt]": "Pesquisa de Contactos PIM",
+        "Name[uk]": "Пошук у записах контактів PIM",
+        "Name[x-test]": "xxPIM Contacts Searchxx",
         "ServiceTypes": [
             "Plasma/Runner"
         ],
