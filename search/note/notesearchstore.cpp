4180f3bf844f5c2f35b31308c72d6f73fabbab2c
Laurent Montel <montel@kde.org>
2023-01-01 18:43:19 +0100

    GIT_SILENT: happy new year

--- search/note/notesearchstore.cpp
+++ search/note/notesearchstore.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of the KDE Akonadi Search Project
- * SPDX-FileCopyrightText: 2014-2022 Laurent Montel <montel@kde.org>
+ * SPDX-FileCopyrightText: 2014-2023 Laurent Montel <montel@kde.org>
  *
  * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
  *
