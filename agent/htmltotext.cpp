8421ace60b1befcc751b977fb06cbe179a93bc95
Carl Schwan <carl.schwan@gnupg.com>
2023-09-27 10:45:46 +0200

    Move convertion from html to text out-of-process
    This will decrease futher the likelihood of the indexer crashing on
    invalid html.
    
    BUG: 474747
    (cherry picked from commit 299c4d3144413edab1d9ff8c0d687f0979cd4c91)

---
+++ agent/htmltotext.cpp
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: 2023 g10 code GmbH
+// SPDX-Contributor: Carl Schwan <carl.schwan@gnupg.com>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#include <QCoreApplication>
+
+#include <KAboutData>
+#include <KLocalizedString>
+#include <QCommandLineParser>
+#include <iostream>
+
+#ifdef Q_OS_WIN32
+#include <fcntl.h>
+#include <io.h>
+#endif
+
+#ifdef HAS_HTMLPARSER
+#include <lib.rs.h>
+#else
+#include <QTextDocument>
+#endif
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    KLocalizedString::setApplicationDomain("akonadi_search");
+
+    KAboutData about(QStringLiteral("akonadi_html_to_text"),
+                     i18n("Akonadi Html To Test"),
+                     QStringLiteral("1.0"),
+                     i18n("Akonadi Html To Test Converter"),
+                     KAboutLicense::LGPL_V2,
+                     i18n("© 2023 KDE Community"));
+
+    about.addAuthor(i18n("Carl Schwan"), i18n("Maintainer"), QStringLiteral("carl@carlschwan.eu"), QStringLiteral("https://carlschwan.eu"));
+
+    KAboutData::setApplicationData(about);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(i18n("Akonadi Html To Test Converter"));
+
+    about.setupCommandLine(&parser);
+    parser.process(app);
+    about.processCommandLine(&parser);
+
+    QByteArray content;
+
+#ifdef Q_OS_WIN32
+    _setmode(_fileno(stdin), _O_BINARY);
+#endif
+
+    while (!std::cin.eof()) {
+        char arr[1024];
+        std::cin.read(arr, sizeof(arr));
+        int s = std::cin.gcount();
+        content.append(arr, s);
+    }
+
+#ifdef HAS_HTMLPARSER
+    const auto html = content.toStdString();
+    const auto text = std::string(convert_to_text(rust::String(html)));
+#else
+    QTextDocument doc;
+    doc.setHtml(QString::fromUtf8(content));
+    const std::string text(doc.toPlainText().toStdString());
+#endif
+
+    std::cout << text;
+
+    return 0;
+}
