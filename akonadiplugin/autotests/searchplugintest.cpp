KDE/akonadi-search

commit b04d33a6c332311ac915c47f4183bd55d34c0129

Port deprecated methods

akonadiplugin/autotests/searchplugintest.cpp
@@ -112,7 +112,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("body2");
-            msg->addContent(b, true);
+            msg->prependContent(b);
 
             msg->from()->addAddress("john@test.com", QStringLiteral("John Doe"));
             msg->to()->addAddress("jane@test.com", QStringLiteral("Jane Doe"));
@@ -136,7 +136,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("body3");
-            msg->addContent(b, true);
+            msg->prependContent(b);
 
             msg->from()->addAddress("john@test.com", QStringLiteral("John Doe"));
             msg->to()->addAddress("jane@test.com", QStringLiteral("Jane Doe"));
@@ -160,7 +160,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("body4");
-            msg->addContent(b, true);
+            msg->prependContent(b);
 
             msg->from()->addAddress("john_blue@test.com", QStringLiteral("John Doe"));
             msg->to()->addAddress("jane@test.com", QStringLiteral("Jane Doe"));
@@ -193,7 +193,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("tags");
-            msg->addContent(b, true);
+            msg->prependContent(b);
 
             msg->from()->addAddress("john@test.com", QStringLiteral("John Doe"));
             msg->to()->addAddress("jane@test.com", QStringLiteral("Jane Doe"));
@@ -227,7 +227,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("body5");
-            msg->addContent(b, true);
+            msg->prependContent(b);
 
             msg->from()->addAddress("john@test.com", QStringLiteral("John Doe"));
             msg->to()->addAddress("jane@test.com", QStringLiteral("Jane Doe"));
@@ -325,7 +325,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("body note");
-            msg->addContent(b, true);
+            msg->prependContent(b);
             msg->assemble();
 
             Akonadi::Item item(QStringLiteral("text/x-vnd.akonadi.note"));
@@ -344,7 +344,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("note");
-            msg->addContent(b, true);
+            msg->prependContent(b);
             msg->assemble();
 
             Akonadi::Item item(QStringLiteral("text/x-vnd.akonadi.note"));
@@ -363,7 +363,7 @@ private Q_SLOTS:
             auto b = new KMime::Content;
             b->contentType()->setMimeType("text/plain");
             b->setBody("note3");
-            msg->addContent(b, true);
+            msg->prependContent(b);
             msg->assemble();
 
             Akonadi::Item item(QStringLiteral("text/x-vnd.akonadi.note"));

commit 6fec74b561b6d85b68e063e2f59b6fe9f7081f61

Fix searchplugintest
Fallout from b04d33a6c332.

akonadiplugin/autotests/searchplugintest.cpp
@@ -106,6 +106,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("subject2");
 
             // Multipart message
@@ -130,6 +131,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("subject3");
 
             // Multipart message
@@ -154,6 +156,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("subject4");
 
             // Multipart message
@@ -187,6 +190,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("all tags");
 
             // Multipart message
@@ -222,6 +226,7 @@ private Q_SLOTS:
         {
             KMime::Message::Ptr msg(new KMime::Message);
             msg->subject()->from7BitString("Change in qt/qtx11extras[stable]: remove QtWidgets dependency");
+            msg->contentType()->setMimeType("multipart/mixed");
 
             // Multipart message
             auto b = new KMime::Content;
@@ -319,6 +324,7 @@ private Q_SLOTS:
         // Note item
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("note");
 
             // Multipart message
@@ -338,6 +344,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("note2");
 
             // Multipart message
@@ -357,6 +364,7 @@ private Q_SLOTS:
         }
         {
             KMime::Message::Ptr msg(new KMime::Message);
+            msg->contentType()->setMimeType("multipart/mixed");
             msg->subject()->from7BitString("note3");
 
             // Multipart message